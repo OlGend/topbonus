83af1c90a1e2d37d267ac44eb6509a55de97f878
Oleh <gend4016@gmail.com>
2024-09-02 09:20:08 +0300

    a

--- .next/server/app/chunks/ssr/components_839128._.js
+++ .next/server/app/chunks/ssr/components_839128._.js
@@ -2439,10 +2439,47 @@ function TopBrands() {
 }
 
 })()),
-"[project]/components/KeitaroIframe.jsx (ecmascript, ssr)": (function({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, k: __turbopack_refresh__, m: module, e: exports }) { !function() {
+"[project]/components/KeitaroIframe.jsx (ecmascript, ssr)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, k: __turbopack_refresh__ }) => (() => {
 
+__turbopack_esm__({
+    "default": ()=>__TURBOPACK__default__export__
+});
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next/dist/compiled/react/jsx-dev-runtime.js (ecmascript, ssr)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/next/dist/compiled/react/index.js (ecmascript, ssr)");
+"__TURBOPACK__ecmascript__hoisting__location__";
+"use client";
+;
+;
+const KeitaroIframe = ()=>{
+    __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$28$ecmascript$29$__["useEffect"](()=>{
+        const keitaroLink = 'https://info.topbon.us/aurnd?keyword=test_oleh_1';
+        const keitaroLink2 = 'https://info.topbon.us/partner_aurnd';
+        const keitaroLink3 = 'https://info.topbon.us/rnd1043';
+        const keitaroLink4 = 'https://info.topbon.us/rnd1044';
+        document.getElementById('keitaro-iframe').src = keitaroLink;
+    }, []);
+    return __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$28$ecmascript$29$__["jsxDEV"]("div", {
+        className: "iframe-container",
+        children: __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$28$ecmascript$29$__["jsxDEV"]("iframe", {
+            id: "keitaro-iframe",
+            src: "",
+            style: {
+                position: 'absolute'
+            }
+        }, void 0, false, {
+            fileName: "<[project]/components/KeitaroIframe.jsx>",
+            lineNumber: 18,
+            columnNumber: 7
+        }, this)
+    }, void 0, false, {
+        fileName: "<[project]/components/KeitaroIframe.jsx>",
+        lineNumber: 17,
+        columnNumber: 5
+    }, this);
+};
+const __TURBOPACK__default__export__ = KeitaroIframe;
 
-}.call(this) }),
+})()),
 "[project]/components/modal/index.jsx (ecmascript, ssr)": (({ r: __turbopack_require__, f: __turbopack_require_context__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, n: __turbopack_export_namespace__, c: __turbopack_cache__, l: __turbopack_load__, j: __turbopack_dynamic__, g: global, __dirname, x: __turbopack_external_require__, y: __turbopack_external_import__, k: __turbopack_refresh__ }) => (() => {
 
 __turbopack_esm__({
@@ -2853,7 +2890,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$react$2d$i18
 async function initializeI18n() {
     let defLng;
     try {
-        const response = await fetch("https://ipapi.co/json/?key=YD0x5VtXrPJkOcFQMjEyQgqjfM6jUcwS4J54b3DI8ztyrFpHzW");
+        const response = await fetch("https://ipapi.co/json");
         const data = await response.json();
         if (typeof window !== "undefined") {
             localStorage.setItem("country", data.country);
